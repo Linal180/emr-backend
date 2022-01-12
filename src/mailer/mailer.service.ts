73f7262bd7115b0ff4d55146e9727d61d9d00dde
Linal180 <amirnisar75@gmail.com>
2022-01-12 19:10:30 +0500

    added forgot password template Key

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -35,8 +35,7 @@ export class MailerService {
    */
   async sendEmailForgotPassword(email: string, userId: string, fullName: string, isAdmin: boolean, token: string, isInvite: boolean) {
     const portalAppBaseUrl = this.configService.get('PORTAL_APP_BASE_URL');
-    const adminAppBaseUrl = this.configService.get('ADMIN_APP_BASE_URL');
-    const url = isAdmin ? `${adminAppBaseUrl}/reset-password?token=${token}` : `${portalAppBaseUrl}/reset-password?token=${token}`
+    const url = `${portalAppBaseUrl}/reset-password?token=${token}`
     const msg = {
       to: email,
       from: this.configService.get('FROM_EMAIL'),
