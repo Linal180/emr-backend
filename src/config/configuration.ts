c30c4c1bf239f509537462d71be0c9063f3615f5
Linal180 <amirnisar75@gmail.com>
2022-01-12 12:21:18 +0500

    addedd ssl option.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,14 +6,15 @@ export default () => {
       type: process.env.DATABASE_TYPE || "postgres",
       url: process.env.DATABASE_URL,
       migrationsRun: true,
+      ssl: true,
       logging: true,
       migrations: ["dist/migrations/*{.ts,.js}"],
       entities: ["dist/**/*.entity{.ts,.js}"],
     };
     redis = {
       socket: {
-        url: process.env.REDIS_URL || "redis://localhost:"
-      }
+        url: process.env.REDIS_URL || "redis://localhost:",
+      },
     };
   } else {
     database = {
@@ -30,9 +31,9 @@ export default () => {
     };
     redis = {
       socket: {
-        host: process.env.REDIS_HOST || '127.0.0.1',
-        port: 6379
-      }
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: 6379,
+      },
     };
   }
 
@@ -47,35 +48,54 @@ export default () => {
     SENDGRID_API_KEY:
       process.env.SENDGRID_API_KEY ||
       "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",
-    INVITATION_TEMPLATE_ID:
-      process.env.INVITATION_TEMPLATE_ID ||
-      "",
-    NOTIFICATION_ADMIN_NEW_SIGN_UP_TEMPLATE_ID: process.env.NOTIFICATION_ADMIN_NEW_SIGN_UP_TEMPLATE_ID || "d-c4f935c3674b4341a10b3c226cea1b86",
-    REQUEST_INITIATED: process.env.REQUEST_INITIATED || "d-156bae7fc5444c7bbbae2044f1d6e97b",
-    UPDATE_DONE: process.env.UPDATE_DONE || "d-6048b558485c4873b1331834356a7a83",
-    REQUEST_APPROVED: process.env.REQUEST_APPROVED || "d-59063472f76c4c5f9acb74cc93d0bf95",
-    REQUEST_UPDATE_NEEDED: process.env.REQUEST_UPDATE_NEEDED || "d-80fb471a2ddc4d2bbf4305a164ff7164",
-    REQUEST_UPDATE_DECLINED: process.env.REQUEST_UPDATE_DECLINED || "d-6343679f0f5448eaacdc0cb0a3ba4edc",
-    FORGOT_PASSWORD_TEMPLATE_ID: process.env.FORGOT_PASSWORD_TEMPLATE_ID || "d-c40af5d836ec4f029f9f9da1f9a06515",
+    INVITATION_TEMPLATE_ID: process.env.INVITATION_TEMPLATE_ID || "",
+    NOTIFICATION_ADMIN_NEW_SIGN_UP_TEMPLATE_ID:
+      process.env.NOTIFICATION_ADMIN_NEW_SIGN_UP_TEMPLATE_ID ||
+      "d-c4f935c3674b4341a10b3c226cea1b86",
+    REQUEST_INITIATED:
+      process.env.REQUEST_INITIATED || "d-156bae7fc5444c7bbbae2044f1d6e97b",
+    UPDATE_DONE:
+      process.env.UPDATE_DONE || "d-6048b558485c4873b1331834356a7a83",
+    REQUEST_APPROVED:
+      process.env.REQUEST_APPROVED || "d-59063472f76c4c5f9acb74cc93d0bf95",
+    REQUEST_UPDATE_NEEDED:
+      process.env.REQUEST_UPDATE_NEEDED || "d-80fb471a2ddc4d2bbf4305a164ff7164",
+    REQUEST_UPDATE_DECLINED:
+      process.env.REQUEST_UPDATE_DECLINED ||
+      "d-6343679f0f5448eaacdc0cb0a3ba4edc",
+    FORGOT_PASSWORD_TEMPLATE_ID:
+      process.env.FORGOT_PASSWORD_TEMPLATE_ID ||
+      "d-c40af5d836ec4f029f9f9da1f9a06515",
     redis,
-    VERIFY_EMAIL_TEMPLATE_ID: process.env.VERIFY_EMAIL_TEMPLATE_ID || "d-0923bc5b5cdb44f9993128a14bbbedd9",
+    VERIFY_EMAIL_TEMPLATE_ID:
+      process.env.VERIFY_EMAIL_TEMPLATE_ID ||
+      "d-0923bc5b5cdb44f9993128a14bbbedd9",
     FROM_EMAIL: process.env.FROM_EMAIL || "ahmad.hassan@alxtel.com",
     database,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || "",
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
     AWS_S3_REGION: process.env.AWS_S3_REGION || "us-east-1",
     AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "boca-plus-private",
-    DOCUSIGN_INTEGERATION_Key: process.env.DOCUSIGN_INTEGERATION_Key || "6627cd91-560c-494f-b274-415af0568361",
-    DOCUSIGN_SECRET_KEY: process.env.DOCUSIGN_SECRET_KEY || "94984962-5186-46d5-8eb8-097c83441b59",
+    DOCUSIGN_INTEGERATION_Key:
+      process.env.DOCUSIGN_INTEGERATION_Key ||
+      "6627cd91-560c-494f-b274-415af0568361",
+    DOCUSIGN_SECRET_KEY:
+      process.env.DOCUSIGN_SECRET_KEY || "94984962-5186-46d5-8eb8-097c83441b59",
     SIGNER_EMAIL: process.env.SIGNER_EMAIL || "tammy@boca-plus.com",
     SIGNER_NAME: process.env.SIGNER_NAME || "Tammy Battistessa",
     REDIRECRI_URI: process.env.REDIRECRI_URI || "http://localhost:3001/",
-    DOCUSIGN_ACCOUNT_ID: process.env.DOCUSIGN_ACCOUNT_ID || "8a8507e3-46a6-427d-b7c2-e26b759d71c8",
-    DOCUSIGN_BASEPATH: process.env.DOCUSIGN_BASEPATH || "https://demo.docusign.net/restapi",
+    DOCUSIGN_ACCOUNT_ID:
+      process.env.DOCUSIGN_ACCOUNT_ID || "8a8507e3-46a6-427d-b7c2-e26b759d71c8",
+    DOCUSIGN_BASEPATH:
+      process.env.DOCUSIGN_BASEPATH || "https://demo.docusign.net/restapi",
     DOCUSIGN_USER_NAME: process.env.DOCUSIGN_USER_NAME || "tammy@boca-plus.com",
-    DOCUSIGN_BASEURL: process.env.DOCUSIGN_BASEURL || "https://appdemo.docusign.com/api/accounts",
+    DOCUSIGN_BASEURL:
+      process.env.DOCUSIGN_BASEURL ||
+      "https://appdemo.docusign.com/api/accounts",
     DOCUSIGN_PASSWORD: process.env.DOCUSIGN_PASSWORD || "BOCAKwanso159!",
     WEBSOCKET_PORT: parseInt(process.env.WEBSOCKET_PORT, 10) || 9352,
-    INSTAGRAM_FEEDS_TOKEN: process.env.INSTAGRAM_FEEDS_TOKEN || "EAADQ7DjtZAZAEBAMzLUGYjtblneLLES7mopZBGFzX0Y30RakWLHTWt6L908SPCeDbNnwxwKBprAUCflomUzvOJotepTwPiyLzOqJ4QU8iWPd2cu3E3MeeAOBZChIe9nTCiGlZAOawrbZAYrZAxHYZCYM8lcO2fjpoYNVLSuTyC45bhoVN8Lz6oz3DfYhITVuWZANlX4FJjVWWghPdSbQXRh2Qd6qOZB0yBoBYZD"
+    INSTAGRAM_FEEDS_TOKEN:
+      process.env.INSTAGRAM_FEEDS_TOKEN ||
+      "EAADQ7DjtZAZAEBAMzLUGYjtblneLLES7mopZBGFzX0Y30RakWLHTWt6L908SPCeDbNnwxwKBprAUCflomUzvOJotepTwPiyLzOqJ4QU8iWPd2cu3E3MeeAOBZChIe9nTCiGlZAOawrbZAYrZAxHYZCYM8lcO2fjpoYNVLSuTyC45bhoVN8Lz6oz3DfYhITVuWZANlX4FJjVWWghPdSbQXRh2Qd6qOZB0yBoBYZD",
   };
 };
