304c5b527a23166e4bf31b2d6f986514a36e1707
Linal180 <amirnisar75@gmail.com>
2022-03-09 16:52:09 +0500

    updateAppointmentStatus mutation added with update in enum of appointment status

--- src/migrations/1646812409542-removedPaymentStatus.ts
+++ src/migrations/1646812409542-removedPaymentStatus.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class removedPaymentStatus1646812409542 implements MigrationInterface {
-    name = 'removedPaymentStatus1646812409542'
+// export class removedPaymentStatus1646812409542 implements MigrationInterface {
+//     name = 'removedPaymentStatus1646812409542'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Appointments" DROP COLUMN "paymentStatus"`);
-    }
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "Appointments" DROP COLUMN "paymentStatus"`);
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Appointments" ADD "paymentStatus" character varying`);
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "Appointments" ADD "paymentStatus" character varying`);
+//     }
 
-}
+// }
