6cd9c0d1ed4cc52700298835bd7c78822f326627
Linal180 <amirnisar75@gmail.com>
2022-03-28 11:48:45 +0500

    fixed permissions seeder

--- src/users/seeds/users.seed.ts
+++ src/users/seeds/users.seed.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { Facility } from "src/facilities/entities/facility.entity";
-import { Connection, getRepository, In } from "typeorm";
+import { Connection, getRepository } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
 import { createPasswordHash } from '../../lib/helper';
 import { Permission } from "../entities/permissions.entity";
@@ -35,159 +35,134 @@ export class CreateUsers implements Seeder {
         permissions = await queryRunner.manager.save(permissions);
       }
       //Add superAdmin role Permissions
-      let superAdminRole = await getRepository(Role).findOne({where: {role: 'super-admin'}});
+      let superAdminRole = roles.find((item)=> item.role === 'super-admin')
       let superAdminRolePermission = await getRepository(RolePermission).find({where: {role: superAdminRole.id}})
       if(!superAdminRolePermission.length){
-      let superAdminPermissionList = await getRepository(Permission).find()
+      let superAdminPermissionList = permissions
       let superAdminRolePermissions = await this.rolePermissionPayload(superAdminPermissionList, superAdminRole)
       let superAdminRolePermissionsRes = getRepository(RolePermission).create(superAdminRolePermissions)
       superAdminRolePermissionsRes = await queryRunner.manager.save(superAdminRolePermissionsRes);
       }
 
       //Add admin role Permissions
-      let adminRole = await getRepository(Role).findOne({where: {role: 'admin'}});
-      let adminRolePermission = await getRepository(RolePermission).find({where: {role: adminRole.id}})
+      let adminRole = roles.find((item)=> item.role === 'admin')
+      let adminRolePermission =  await getRepository(RolePermission).find({where: {role: adminRole.id}});
       if(!adminRolePermission.length){
-      let adminPermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
-            'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
-            'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
-            'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
-            'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','updatePatientProvider','findAllPatient','getPatient','createPractice',
-            'updatePractice','getPractice','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
-        console.log("adminPermissionList",adminPermissionList);
+      let adminPermissionSet =  ['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
+      'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
+      'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
+      'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
+      'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','updatePatientProvider','findAllPatient','getPatient','createPractice',
+      'updatePractice','getPractice','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
+      'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let adminPermissionList = permissions.filter(x => adminPermissionSet.find(y => (y === x.name)));
       let adminRolePermissions = await this.rolePermissionPayload(adminPermissionList, adminRole)
       let adminRolePermissionsRes = getRepository(RolePermission).create(adminRolePermissions)
       adminRolePermissionsRes = await queryRunner.manager.save(adminRolePermissionsRes);
       }
-
       //Add doctor role Permissions
-      let doctorRole = await getRepository(Role).findOne({where: {role: 'doctor'}});
+      let doctorRole = roles.find((item)=> item.role === 'doctor')
       let doctorRolePermission = await getRepository(RolePermission).find({where: {role: doctorRole.id}})
       if(!doctorRolePermission.length){
-      let doctorPermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
+      let doctorPermissionSet = ['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
             'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
             'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
             'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
             'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
+            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let doctorPermissionList = permissions.filter(x => doctorPermissionSet.find(y => (y === x.name)));
       let doctorRolePermissions = await this.rolePermissionPayload(doctorPermissionList, doctorRole)
       let doctorRolePermissionsRes = getRepository(RolePermission).create(doctorRolePermissions)
       doctorRolePermissionsRes = await queryRunner.manager.save(doctorRolePermissionsRes);
       }
 
       //Add nurse role Permissions
-      let nurseRole = await getRepository(Role).findOne({where: {role: 'nurse'}});
+      let nurseRole = roles.find((item)=> item.role === 'nurse')
       let nurseRolePermission = await getRepository(RolePermission).find({where: {role: nurseRole.id}})
       if(!nurseRolePermission.length){
-      let nursePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
+      let nursePermissionSet = ['createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
             'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
             'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
             'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
+            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let nursePermissionList = permissions.filter(x => nursePermissionSet.find(y => (y === x.name)));
       let nurseRolePermissions = await this.rolePermissionPayload(nursePermissionList, nurseRole)
       let nurseRolePermissionsRes = getRepository(RolePermission).create(nurseRolePermissions)
       nurseRolePermissionsRes = await queryRunner.manager.save(nurseRolePermissionsRes);
       }
 
       //Add patient role Permissions
-      let patientRole = await getRepository(Role).findOne({where: {role: 'patient'}});
+      let patientRole = roles.find((item)=> item.role === 'patient')
       let patientRolePermission = await getRepository(RolePermission).find({where: {role: patientRole.id}})
       if(!patientRolePermission.length){
-      let patientRolePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
+      let patientRolePermissionSet = ['createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
             'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
             'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
             'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
+            'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let patientRolePermissionList = permissions.filter(x => patientRolePermissionSet.find(y => (y === x.name)));
       let patientRolePermissions = await this.rolePermissionPayload(patientRolePermissionList, patientRole)
       let patientRolePermissionsRes = getRepository(RolePermission).create(patientRolePermissions)
       patientRolePermissionsRes = await queryRunner.manager.save(patientRolePermissionsRes);
       }
 
       //Add staff role Permissions
-      let staffRole = await getRepository(Role).findOne({where: {role: 'staff'}});
+      let staffRole = roles.find((item)=> item.role === 'staff')
       let staffRolePermission = await getRepository(RolePermission).find({where: {role: staffRole.id}})
       if(!staffRolePermission.length){
-      let staffRolePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['createPatient','updatePatient','patientInfo','updatePatientProfile',
+      let staffRolePermissionSet = ['createPatient','updatePatient','patientInfo','updatePatientProfile',
              'findAllPatient','getPatient','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule',
-             'getDoctorSlots','findAllDoctor','getDoctor'])
-         }
-        })
+             'getDoctorSlots','findAllDoctor','getDoctor']
+      let staffRolePermissionList = permissions.filter(x => staffRolePermissionSet.find(y => (y === x.name)));  
+      console.log("staffRolePermissionList",staffRolePermissionList);
       let staffRolePermissions = await this.rolePermissionPayload(staffRolePermissionList, staffRole)
       let staffRolePermissionsRes = getRepository(RolePermission).create(staffRolePermissions)
       staffRolePermissionsRes = await queryRunner.manager.save(staffRolePermissionsRes);
       }
 
       //Add nursePractitioner role Permissions
-      let nursePractitionerRole = await getRepository(Role).findOne({where: {role: 'nurse_practitioner'}});
+      let nursePractitionerRole = roles.find((item)=> item.role === 'nurse_practitioner')
       let nursePractitionerRolePermission = await getRepository(RolePermission).find({where: {role: nursePractitionerRole.id}})
       if(!nursePractitionerRolePermission.length){
-      let nursePractitionerRolePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
+      let nursePractitionerRolePermissionSet = ['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
              'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
              'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
              'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
              'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-             'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
+             'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let nursePractitionerRolePermissionList = permissions.filter(x => nursePractitionerRolePermissionSet.find(y => (y === x.name)));  
       let nursePractitionerRolePermissions = await this.rolePermissionPayload(nursePractitionerRolePermissionList, nursePractitionerRole)
       let nursePractitionerRolePermissionsRes = getRepository(RolePermission).create(nursePractitionerRolePermissions)
       nursePractitionerRolePermissionsRes = await queryRunner.manager.save(nursePractitionerRolePermissionsRes);
       }
       
       //Add office manager role Permissions
-      let officeManagerRole = await getRepository(Role).findOne({where: {role: 'office_manager'}});
+      let officeManagerRole = roles.find((item)=> item.role === 'office_manager')
       let officeManagerRolePermission = await getRepository(RolePermission).find({where: {role: officeManagerRole.id}})
       if(!officeManagerRolePermission.length){
-      let officeManagerRolePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
+      let officeManagerRolePermissionSet = ['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
              'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
              'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
              'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
              'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-             'findAllStaff','getStaff','removeStaff','disableStaff'])
-         }
-        })
+             'findAllStaff','getStaff','removeStaff','disableStaff']
+      let officeManagerRolePermissionList =  permissions.filter(x => officeManagerRolePermissionSet.find(y => (y === x.name)));    
       let officeManagerRolePermissions = await this.rolePermissionPayload(officeManagerRolePermissionList, officeManagerRole)
       let officeManagerRolePermissionsRes = getRepository(RolePermission).create(officeManagerRolePermissions)
       officeManagerRolePermissionsRes = await queryRunner.manager.save(officeManagerRolePermissionsRes);
       }
 
       //Add office doctor assistant Permissions
-      let doctorAssistantRole = await getRepository(Role).findOne({where: {role: 'doctor_assistant'}});
+      let doctorAssistantRole = roles.find((item)=> item.role === 'doctor_assistant')
       let doctorAssistantRolePermission = await getRepository(RolePermission).find({where: {role: doctorAssistantRole.id}})
       if(!doctorAssistantRolePermission.length){
-      let doctorAssistantRolePermissionList = await getRepository(Permission).find({
-          where: {
-             name: In(['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
+      let doctorAssistantRolePermissionSet = ['fetchAllUsers','fetchUser','getUser','searchUser','forgotPassword','deactivateUser','updateUser','updateRole','createRole',
              'updateRole','getAllRoles','getRole','createAppointment','createExternalAppointment','updateAppointment','updateAppointmentBillingStatus',
              'findAllAppointments','getAppointment','getDoctorAppointment','cancelAppointment','removeAppointment','getPatientAppointment','createFacility','updateFacility',
              'updateFacilityTimeZone','findAllFacility','getFacility','createService','updateService','findAllServices','getService','createPatient',
              'updatePatient','patientInfo','updatePatientProfile','sendInviteToPatient','findAllPatient','getPatient','createDoctor','updateDoctor','findAllDoctor','getDoctor','removeDoctor','disableDoctor','createStaff','updateStaff',
-             'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule'])
-         }
-        })
+             'findAllStaff','getStaff','removeStaff','disableStaff','createSchedule','updateSchedule','findAllSchedules','getDoctorSchedule','getDoctorSlots','removeSchedule']
+      let doctorAssistantRolePermissionList =  permissions.filter(x => doctorAssistantRolePermissionSet.find(y => (y === x.name)));  
       let doctorAssistantRolePermissions = await this.rolePermissionPayload(doctorAssistantRolePermissionList, doctorAssistantRole)
       let doctorAssistantRolePermissionsRes = getRepository(RolePermission).create(doctorAssistantRolePermissions)
       doctorAssistantRolePermissionsRes = await queryRunner.manager.save(doctorAssistantRolePermissionsRes);
