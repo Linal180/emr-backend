1f2f07d9d5410688a189baa808e352222ea32525
Linal180 <amirnisar75@gmail.com>
2022-04-01 18:10:45 +0500

    removed useless  methods

--- src/formbuilder/services/element.service.ts
+++ src/formbuilder/services/element.service.ts
@@ -7,44 +7,38 @@ import { ElementInputs } from "../dto/element.input";
 //service
 @Injectable()
 export class ElementService {
+  
+  constructor(@InjectRepository(Element) private elementRepository: Repository<Element>) { }
+  
+  /**
+   * Creates element service
+   * @param inputs 
+   * @returns  
+   */
+  async create(inputs: ElementInputs) {
+    try {
+      const element = this.elementRepository.create(inputs);
+      return await this.elementRepository.save(element)
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 
-    constructor(@InjectRepository(Element) private elementRepository: Repository<Element>) { }
-
-    //create element
-    async create(inputs: ElementInputs) {
-        try {
-            const element = this.elementRepository.create(inputs);
-            return await this.elementRepository.save(element)
-        } catch (error) {
-            throw new InternalServerErrorException(error);
+  /**
+   * Gets by type
+   * @param type 
+   * @returns  
+   */
+  async getByType(type: ElementInputs) {
+    try {
+      return await this.elementRepository.findOneOrFail({
+        where: {
+          type: type.type
         }
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-
-    //get element by id
-    async getById() {
-
-    }
-
-    //get element by type
-    async getByType(type: ElementInputs) {
-        try {
-            return await this.elementRepository.findOneOrFail({
-                where: {
-                    type: type.type
-                }
-            })
-        } catch (error) {
-            throw new InternalServerErrorException(error);
-        }
-    }
-
-    //update element
-    async update() {
-
-    }
-
-    //get all elements
-    async getAll() {
-
-    }
+  }
+  
 }
