005bbd9e6cf9429cf65a7afac39571264743cbc8
Linal180 <amirnisar75@gmail.com>
2022-04-01 18:09:47 +0500

    is deleted  flag added in form element

--- src/formbuilder/entities/form-elements.entity.ts
+++ src/formbuilder/entities/form-elements.entity.ts
@@ -47,9 +47,9 @@ export class FormElement {
   @Field({ nullable: false })
   sectionId: string;
 
-  // @Column({ default: false })
-  // @Field(() => Boolean)
-  // isDeleted: boolean;
+  @Column({ default: false })
+  @Field(() => Boolean)
+  isDeleted: boolean;
 
   @ManyToOne(() => Form, form => form.formElements, { onDelete: "CASCADE" })
   form: Form;
