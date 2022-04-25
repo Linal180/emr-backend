5e248f22af8d410738e4f94af15c4dde4a9d2a18
Linal180 <amirnisar75@gmail.com>
2022-04-25 15:33:16 +0500

    make separate form seed

--- src/formbuilder/seeds/elements.seed.ts
+++ src/formbuilder/seeds/elements.seed.ts
@@ -3,8 +3,7 @@ import { Connection, getRepository } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
 //user imports
 import { Element } from '../entities/element.entity';
-import { Form } from "../entities/form.entity";
-import { ElementTypeData, FormTemplates } from '../seeds/seed-data'
+import { ElementTypeData } from '../seeds/seed-data'
 //class
 @Injectable()
 export class CreateElements implements Seeder {
@@ -18,32 +17,6 @@ export class CreateElements implements Seeder {
 				for (let index = 0; index < ElementTypeData.length; index++) {
 					const element = ElementTypeData[index];
 					const eleObj = getRepository(Element).create(element)
-					await queryRunner.manager.save(eleObj);
-				}
-			}
-			await queryRunner.commitTransaction();
-		}
-		catch (error) {
-			await queryRunner.rollbackTransaction();
-			throw new InternalServerErrorException(error);
-		} finally {
-			await queryRunner.release();
-		}
-	}
-}
-
-@Injectable()
-export class CreateFormTemplate implements Seeder {
-	public async run(factory: Factory, connection: Connection): Promise<void> {
-		const queryRunner = connection.createQueryRunner();
-		await queryRunner.connect();
-		await queryRunner.startTransaction();
-		try {
-			const elements = await getRepository(Form).find();
-			if (!elements.length) {
-				for (let index = 0; index < FormTemplates.length; index++) {
-					const element = FormTemplates[index];
-					const eleObj = getRepository(Form).create(element)
 					await queryRunner.manager.save(eleObj);
 				}
 			}
