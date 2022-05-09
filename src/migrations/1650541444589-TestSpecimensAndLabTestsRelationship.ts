71e667dca818d4f3cbeed72ce5a68a1f13d126d7
Linal180 <amirnisar75@gmail.com>
2022-05-09 15:49:16 +0500

    commented migration LabTests DROP CONSTRAINT FK_2090bc39e00268af131d1b8b108

--- src/migrations/1650541444589-TestSpecimensAndLabTestsRelationship.ts
+++ src/migrations/1650541444589-TestSpecimensAndLabTestsRelationship.ts
@@ -5,7 +5,7 @@ export class TestSpecimensAndLabTestsRelationship1650541444589 implements Migrat
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // await queryRunner.query(`ALTER TABLE "LabTests" DROP CONSTRAINT "FK_dca5c59d1c6fc81b2cc2c938e41"`);
-        await queryRunner.query(`ALTER TABLE "LabTests" DROP COLUMN "testSpecimensId"`);
+        // await queryRunner.query(`ALTER TABLE "LabTests" DROP COLUMN "testSpecimensId"`);
         await queryRunner.query(`ALTER TABLE "TestSpecimens" ADD "labTestId" uuid`);
         await queryRunner.query(`ALTER TABLE "TestSpecimens" ADD CONSTRAINT "FK_0602065f263d9ea302b706908d9" FOREIGN KEY ("labTestId") REFERENCES "LabTests"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
@@ -13,7 +13,7 @@ export class TestSpecimensAndLabTestsRelationship1650541444589 implements Migrat
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "TestSpecimens" DROP CONSTRAINT "FK_0602065f263d9ea302b706908d9"`);
         await queryRunner.query(`ALTER TABLE "TestSpecimens" DROP COLUMN "labTestId"`);
-        await queryRunner.query(`ALTER TABLE "LabTests" ADD "testSpecimensId" uuid`);
+        // await queryRunner.query(`ALTER TABLE "LabTests" ADD "testSpecimensId" uuid`);
         // await queryRunner.query(`ALTER TABLE "LabTests" ADD CONSTRAINT "FK_dca5c59d1c6fc81b2cc2c938e41" FOREIGN KEY ("testSpecimensId") REFERENCES "TestSpecimens"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
