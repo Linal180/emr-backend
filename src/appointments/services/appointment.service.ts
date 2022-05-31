b90e6809635b3e51c96ad33e36945b0f5e34e1d7
Linal180 <amirnisar75@gmail.com>
2022-05-31 20:05:24 +0500

    Appointment status updated

--- src/appointments/services/appointment.service.ts
+++ src/appointments/services/appointment.service.ts
@@ -21,7 +21,7 @@ import { AppointmentsPayload } from '../dto/appointments-payload.dto';
 import { CreateAppointmentInput } from '../dto/create-appointment.input';
 import { CreateExternalAppointmentInput } from '../dto/create-external-appointment.input';
 import { CancelAppointment, GetAppointments, GetFacilityAppointmentsInput, GetPatientAppointmentInput, RemoveAppointment, UpdateAppointmentBillingStatusInput, UpdateAppointmentInput, UpdateAppointmentStatusInput } from '../dto/update-appointment.input';
-import { Appointment, APPOINTMENTSTATUS, BillingStatus } from '../entities/appointment.entity';
+import { Appointment, AppointmentStatus, BillingStatus } from '../entities/appointment.entity';
 
 @Injectable()
 export class AppointmentService {
@@ -292,7 +292,7 @@ export class AppointmentService {
   async findAppointment(providerId: string, patientId: string) {
     return await this.appointmentRepository.findOne({
       where: [
-        { patientId: patientId, providerId: providerId, status: APPOINTMENTSTATUS.INITIATED }
+        { patientId: patientId, providerId: providerId, status: AppointmentStatus.INITIATED }
       ]
     });
   }
@@ -317,7 +317,7 @@ export class AppointmentService {
     if (getAppointments.doctorId) {
       const appointment = await this.appointmentRepository.find({
         where: [
-          { providerId: getAppointments.doctorId, status: APPOINTMENTSTATUS.INITIATED }
+          { providerId: getAppointments.doctorId, status: AppointmentStatus.INITIATED }
         ]
       })
       if (appointment) {
@@ -326,7 +326,7 @@ export class AppointmentService {
     } else if (getAppointments.facilityId) {
       const appointment = await this.appointmentRepository.find({
         where: [
-          { facilityId: getAppointments.facilityId, status: APPOINTMENTSTATUS.INITIATED }
+          { facilityId: getAppointments.facilityId, status: AppointmentStatus.INITIATED }
         ]
       })
       if (appointment) {
@@ -411,7 +411,7 @@ export class AppointmentService {
           }
           await this.updateAppointmentBillingStatus({ id: appointment.id, billingStatus: BillingStatus.REFUND })
         }
-        return await this.appointmentRepository.save({ id: appointment.id, status: APPOINTMENTSTATUS.CANCELLED, token: '', reason: cancelAppointment.reason, })
+        return await this.appointmentRepository.save({ id: appointment.id, status: AppointmentStatus.CANCELLED, token: '', reason: cancelAppointment.reason, })
       }
       throw new NotFoundException({
         status: HttpStatus.NOT_FOUND,
