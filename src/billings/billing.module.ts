364423b3f976241ff39011182ae7b2184bb40a11
Linal180 <amirnisar75@gmail.com>
2022-07-25 17:34:43 +0500

    Claim And Super Bill Implementation

--- src/billings/billing.module.ts
+++ src/billings/billing.module.ts
@@ -1,22 +1,23 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppointmentModule } from 'src/appointments/appointment.module';
 import { FacilityModule } from 'src/facilities/facility.module';
+import { FeeScheduleModule } from 'src/feeSchedule/feeSchedule.module';
 import { InsuranceModule } from 'src/insurance/insurance.module';
-import { PolicyHolderService } from 'src/insurance/services/policy-holder.service';
+import { PaginationModule } from 'src/pagination/pagination.module';
 import { PatientModule } from 'src/patients/patient.module';
+import { PracticeModule } from 'src/practice/practice.module';
 import { ProviderModule } from 'src/providers/provider.module';
+import { UsersModule } from 'src/users/users.module';
 import { Billing } from './entities/billing.entity';
+import { ClaimStatus } from './entities/claim-status.entity';
 import { Code } from './entities/code.entity';
 import { BillingResolver } from './resolvers/billing.resolver';
-import { BillingService } from './services/billing.service';
-import { UsersModule } from 'src/users/users.module';
-import { PracticeModule } from 'src/practice/practice.module';
 import { ClaimStatusResolver } from './resolvers/claimStatus.resolver';
+import { CodeResolver } from './resolvers/codes.resolver';
+import { BillingService } from './services/billing.service';
 import { ClaimStatusService } from './services/claimStatus.service';
-import { ClaimStatus } from './entities/claim-status.entity';
-import { PaginationModule } from 'src/pagination/pagination.module';
 
 @Module({
   imports: [
@@ -29,9 +30,10 @@ import { PaginationModule } from 'src/pagination/pagination.module';
     FacilityModule,
     PracticeModule,
     PaginationModule,
+    FeeScheduleModule,
     HttpModule
   ],
-  providers: [BillingResolver, BillingService, ClaimStatusResolver, ClaimStatusService],
+  providers: [BillingResolver, BillingService, ClaimStatusResolver, ClaimStatusService, CodeResolver],
   exports: [TypeOrmModule],
 })
 export class BillingModule { }
