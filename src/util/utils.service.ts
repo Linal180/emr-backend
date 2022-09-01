81e1fae01fe0e584a26bc44002ab5c16399e5493
Linal180 <amirnisar75@gmail.com>
2022-09-01 21:17:01 +0500

    added some debugging logs

--- src/util/utils.service.ts
+++ src/util/utils.service.ts
@@ -61,9 +61,10 @@ export class UtilsService {
    * @param twilioInput 
    */
   async smsNotification(twilioInput: TwilioInput) {
+    console.log({twilioInput});
     const response = await client.messages.create({
       body: twilioInput.body,
-      from: process.env.TWILIO_PHONE_NUMBER,
+      from: '+19382015325',//process.env.TWILIO_PHONE_NUMBER,
       to: twilioInput.to
     });
     return response
